2ba85f469e336692b84573d25c79145bcd0ed629
Maxime Emonnot <61348720+MaximeEmonnot@users.noreply.github.com>
2023-11-12 18:11:46 +0100

    Refonte des boîtes à moustache - Ligue 1

--- Sources/Description_Variables_Ligue1.R
+++ Sources/Description_Variables_Ligue1.R
@@ -1,5 +1,5 @@
 # Ouverture du fichier .csv
-DataFootball <- read.csv(file.choose(), header=T, sep=",")
+DataFootball <- read.csv(file.choose(), header=T, sep=",", row.names=1)
 
 # Données Ligue 1
 DataFootball_Ligue1 <- DataFootball[DataFootball$Competition == "fr Ligue 1", -which(names(DataFootball) == "Competition")]
@@ -10,5 +10,9 @@ summary(DataFootball_Ligue1)
 ## Boîtes à moustache
 for(i in 1:ncol(DataFootball_Ligue1))
 {
-  boxplot(DataFootball_Ligue1[,i])
+  png(file = paste("./Diagrammes/Description_Variables_Ligue1/", colnames(DataFootball_Ligue1)[i], ".png"),
+      width = 1920,
+      height = 1080)
+  boxplot(DataFootball_Ligue1[,i], main=colnames(DataFootball_Ligue1)[i])
+  dev.off()
 }
